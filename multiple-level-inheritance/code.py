84025526fa76102b57aa9c35a27ccd6eb1acd7c8
Mehedi Mim <mehedi.mim.bd@gmail.com>
2022-02-17 16:51:20 +0600

    multilevel inheritence 2

--- multiple-level-inheritance/code.py
+++ multiple-level-inheritance/code.py
@@ -1,10 +1,18 @@
 class Father:
+    def __init__(self):
+        print("Father")
     def showF(self):
         print("Father Class Method")
 class Son(Father):
+    def __init__(self):
+        print("Son")
     def shows(self):
         print("Son Class Method")
 
 class GrandSon(Son):
+    def __init__(self):
+        print("GrandSon")
     def showG(self):
         print("GrandSon Class Method")
+
+g = GrandSon()
