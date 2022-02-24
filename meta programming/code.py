1b371ab84ccbbed13e8f0fe38ce35a8c2a729d40
Mehedi Mim <mehedi.mim.bd@gmail.com>
2022-02-24 20:17:27 +0600

    Code implemented

--- meta programming/code.py
+++ meta programming/code.py
@@ -1,4 +1,3 @@
-import time
 # Define a class
 class Fruit:
     pass
@@ -22,3 +21,55 @@ import copy
 
 Fruit2 = copy.deepcopy(Fruit)
 print(Fruit2)
+
+
+def give():
+    class Something:
+        pass
+    return Something
+
+# Example call
+s = give()
+print(s)
+
+Fruit = type('Fruit',(),{})
+banana = Fruit()
+print(banana)
+
+Fruit = type('Fruit', (), {'color': 'Green'})
+banana = Fruit()
+print(banana.color)
+
+def info(self):
+    print(self.color)
+
+Fruit = type('Fruit', (), {'color': 'Green', 'info': info})
+apple = Fruit()
+apple.info()
+
+
+class Organic:
+    pass
+
+Fruit = type('Fruit', (Organic, ), {})
+
+
+class Meta(type):
+    def __new__(self, class_name, bases, attrs):
+        modified_attrs = {}
+        for name,value in attrs.items():
+            if name.startswith("__"):
+                modified_attrs[name] = value
+            else:
+                modified_attrs[name.upper()] = value
+        return type(class_name, bases,modified_attrs)
+
+class Fruit(metaclass=Meta):
+    def  myfunc(self):
+        return "Hiiiiii"
+    color = "Green"
+    
+    
+f = Fruit()
+print(f.COLOR)
+print(f.MYFUNC())
